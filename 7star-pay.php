8d342b8605beee76022951fe3acef265fc1b4825
Ken Qiu <kenin.qiu@gmail.com>
2021-09-28 11:16:17 -0400

    change back to production

--- 7star-pay.php
+++ 7star-pay.php
@@ -31,10 +31,8 @@
     define('C_WC_7STARPAY_DIR',rtrim(plugin_dir_path(__FILE__),'/'));
     define('C_WC_7STARPAY_URL',rtrim(plugin_dir_url(__FILE__),'/'));
 
-//    define('C_WC_7STARPAY_OPENAPI_HOST','https://api.blockchaingate.com/v2/');
-//    define('C_WC_7STARPAY_WEB', 'https://www.madearn.com/wallet/bindpay');
-    define('C_WC_7STARPAY_OPENAPI_HOST','https://test.blockchaingate.com/v2/');
-    define('C_WC_7STARPAY_WEB', 'http://localhost:4200/wallet/bindpay');
+    define('C_WC_7STARPAY_OPENAPI_HOST','https://api.blockchaingate.com/v2/');
+    define('C_WC_7STARPAY_WEB', 'https://www.madearn.com/wallet/bindpay');
     add_action( 'plugins_loaded', 'init_7star_pay_gateway_class' );
 
     function init_7star_pay_gateway_class() {
