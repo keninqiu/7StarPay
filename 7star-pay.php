ff64e660c35075229602dbcf7dc8a766b5a9923c
Ken Qiu <kenin.qiu@gmail.com>
2021-09-27 22:44:50 -0400

    wp_add_inline_script

--- 7star-pay.php
+++ 7star-pay.php
@@ -83,7 +83,7 @@
     function wc_7starpay_widget_enqueue_script() {   
         $qrcode = 'haha';
         wp_enqueue_script( 'qrcode_script', plugin_dir_url( __FILE__ ) . 'js/qrcode.min.js' );
-        $codeScript = 'var qrcode = new QRCode(document.getElementById("code"), {width : 200,height : 200});'.'qrcode.makeCode(' . $qrcode . ')';
+        $codeScript = 'var qrcode = new QRCode(document.getElementById("code"), {width : 200,height : 200});'.'qrcode.makeCode("' . $qrcode . '")';
         wp_add_inline_script( 'qrcode_script', $codeScript );
     }
     add_action('wp_footer', 'wc_7starpay_widget_enqueue_script');
