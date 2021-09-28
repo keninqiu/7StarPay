fd4032f87b2b61ae4c4585aff656a77419e51109
Ken Qiu <kenin.qiu@gmail.com>
2021-09-27 21:30:53 -0400

    wp_add_inline_script

--- class-wc-7star-pay-gateway.php
+++ class-wc-7star-pay-gateway.php
@@ -198,7 +198,13 @@ class WC_7StarPay_Gateway extends WC_Payment_Gateway {
                         <div id="code" class="codestyle"></div>
 
 <?php 
-wp_add_inline_script( 'generate-qrcode', 'var qrcode = new QRCode(document.getElementById("code"), {width : 200,height : 200});   ' );
+    function wc_7starpay_widget_enqueue_script() {   
+        wp_enqueue_script( 'qrcode_script', plugin_dir_url( __FILE__ ) . 'js/qrcode.min.js' );
+        wp_add_inline_script( 'generate-qrcode', 'var qrcode = new QRCode(document.getElementById("code"), {width : 200,height : 200});' );
+    }
+    add_action('wp_enqueue_scripts', 'wc_7starpay_widget_enqueue_script');
+
+
 ?>
 
                         <script type="text/javascript">
